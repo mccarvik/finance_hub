35779125224aae35fbb0555ee9c49520e37fa3ac
mccarvik <mccarviks@gmail.com>
2016-12-02 19:05:46 +0000

    added conversion factor calc

--- app/bond/bond_fixed.py
+++ app/bond/bond_fixed.py
@@ -1,14 +1,21 @@
+import sys
+sys.path.append("/home/ubuntu/workspace/finance")
 import datetime
+from app import app
+from app.utils.helper_funcs import cumPresentValue, createCashFlows
 
 class FixedRateBond():
     """This class will hold all the variables associated with a fixed rate bond"""
     
-    def __init__(self, mat_dt, issue_dt, freq=None, cpn=None, dcc=None, par=None, price=None, ytm=None):
-        self._mat_dt = mat_dt
+    def __init__(self, tenor, issue_dt, freq=None, cpn=None, dcc=None, par=None, price=None, ytm=None):
         self._issue_dt = issue_dt
+        self._tenor = tenor
+        self._mat_dt = issue_dt + datetime.timedelta(tenor)
+        self._trade_dt = issue_dt       # for now
+        self._settle_dt = issue_dt      # for convenience
         self._dcc = dcc or "ACT/ACT"
-        self._cpn = cpn or 0     # expressed in percent terms
-        self._pay_freq = freq or "0.5"
+        self._cpn = cpn or 0            # expressed in percent terms, ex: 0.02 = 2%
+        self._pay_freq = freq or "0.5"  # expressed in fractional terms of 1 year
         self._par = par or 100
         if price:
             self._price = price
@@ -17,9 +24,19 @@ class FixedRateBond():
             self._ytm = ytm
             # self._price = self.calcPriceFromYTM()
         
-        self._cash_flows = self.createCashFlows()
+        self._cash_flows = createCashFlows(self._issue_dt, self._pay_freq, self._tenor, self._cpn, self._par)
+        self._conv_factor = self.calcConversionFactor()
     
-    
-    def createCashFlows(self):
-        pass
+    def calcConversionFactor(self):
+        # Assumptions: 20 yrs to maturity, 6% annual disc rate, semi-annual compounding, first cpn payment in 6 months
+        cfs = createCashFlows(self._issue_dt, 0.5, 20, self._cpn, 100)
+        return cumPresentValue(self._trade_dt, 0.06, cfs, 0.5) / self._par
+        
+        
+        
+
+if __name__ == "__main__":
+    # import pdb; pdb.set_trace()
+    bond = FixedRateBond(2, datetime.date(2016,3,1), 0.5, 0.10, "ACT/ACT", 100, 98)
+    print(bond._conv_factor)
     
--- app/utils/helper_funcs.py
+++ app/utils/helper_funcs.py
@@ -1,3 +1,4 @@
+import datetime
 
 def stringify(strs):
     if isinstance(strs, list):
@@ -22,9 +23,31 @@ def bootstrap(first_zero_rate, first_mat, bs_rate_mats):
             (bs_rate_mats[i][1] + new_bs_rate_mats[-1][0])
         new_bs_rate_mats.append(tuple([bs_rate_mats[i][1] + new_bs_rate_mats[-1][0], next_bs_zero_rate]))
     return new_bs_rate_mats
+
+
+def cumPresentValue(today, annual_disc_rate, cfs, freq=1):
+    cum_pv = 0
+    # freq if rate passed in is not annual, ex: 0.5 = semiannual
+    ir = annual_disc_rate * freq
+    for i in cfs:
+        period = ((i[0] - today).days / 365) / freq
+        cum_pv += calcPV(i[1], ir, period)
+    return cum_pv
+
+def calcPV(cf, ir, period):
+    return (cf / (1+ir)**period)
+
+
+def createCashFlows(start_date, freq, tenor, cpn, par):
+    num_cfs = (1 / freq) * tenor
+    days_from_issue = [int((365 * freq)*(i+1)) for i in range(int(num_cfs))]
+    dates = [start_date + datetime.timedelta(i) for i in days_from_issue]
+    cfs = [(dates[i], cpn * par * freq) for i in range(len(dates))]
+    cfs[-1] = (cfs[-1][0], cfs[-1][-1] + par)
+    return cfs
         
     
     
 if __name__ == "__main__":
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     print(bootstrap(0.048, 400, [(0.053, 91), (0.055, 98)]))
