f5063ccfe4f0bc503ebc4275da99decf880a0ea6
mccarvik <mccarviks@gmail.com>
2016-12-28 19:44:24 +0000

    more option stuff

--- app/utils/fi_funcs.py
+++ app/utils/fi_funcs.py
@@ -1,6 +1,10 @@
-import datetime
+import datetime, sys
 from scipy import optimize
 from math import sqrt, pi, log, e
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pandas.io.data import DataReader
 
 
 def bootstrap(first_zero_rate, first_mat, bs_rate_mats):
@@ -75,6 +79,71 @@ def newton_raphson(func, guess, rng=0.0001):
         lastX = nextX
         nextX = lastX - newY / derivative(func, lastX, rng)  # update estimate using N-R
     return nextX
+
+def VaR(notl, conf=0.95, dist=None):
+    ###############################################################################
+    # Retrieve the data from Internet
+    
+    # Choose a time period
+    d1 = datetime.datetime(2001, 1, 1)
+    d2 = datetime.datetime(2012, 1, 1)
+    
+    #get the tickers
+    price = DataReader("AAPL", "yahoo",d1,d2)['Adj Close']
+    price = price.asfreq('B').fillna(method='pad')
+    ret = price.pct_change()
+    
+    #choose the quantile
+    quantile=1-conf
+    #the vol window
+    volwindow=50
+    #and the Var window for rolling 
+    varwindow=250
+    
+    #simple VaR using all the data
+    unnormedquantile=pd.expanding_quantile(ret,quantile)
+    
+    #similar one using a rolling window 
+    unnormedquantileR=pd.rolling_quantile(ret,varwindow,quantile)
+    
+    #we can also normalize the returns by the vol
+    vol=pd.rolling_std(ret,volwindow)*np.sqrt(256)
+    unitvol=ret/vol
+    
+    #and get the expanding or rolling quantiles
+    Var=pd.expanding_quantile(unitvol,quantile)
+    VarR=pd.rolling_quantile(unitvol,varwindow,quantile)
+    
+    normedquantile=Var*vol
+    normedquantileR=VarR*vol
+    
+    ret2=ret.shift(-1)
+    
+    courbe=pd.DataFrame({'returns':ret2,
+                  'quantiles':unnormedquantile,
+                  'Rolling quantiles':unnormedquantileR,
+                  'Normed quantiles':normedquantile,
+                  'Rolling Normed quantiles':normedquantileR,
+                  })
+    
+    courbe['nqBreak']=np.sign(ret2-normedquantile)/(-2) +0.5
+    courbe['nqBreakR']=np.sign(ret2-normedquantileR)/(-2) +0.5
+    courbe['UnqBreak']=np.sign(ret2-unnormedquantile)/(-2) +0.5
+    courbe['UnqBreakR']=np.sign(ret2-unnormedquantileR)/(-2) +0.5
+    
+    
+    nbdays=price.count()
+    
+    print('Number of returns worse than the VaR')
+    print('Ideal Var                : ', (quantile)*nbdays)
+    print('Simple VaR               : ', np.sum(courbe['UnqBreak']))
+    print('Normalized VaR           : ', np.sum(courbe['nqBreak']))
+    print('---------------------------')
+    print('Ideal Rolling Var        : ', (quantile)*(nbdays-varwindow))
+    print('Rolling VaR              : ', np.sum(courbe['UnqBreakR']))
+    print('Rolling Normalized VaR   : ', np.sum(courbe['nqBreakR']))
+    # If no distribution provided, assume normal distribution
+    pass
     
     
 if __name__ == "__main__":
