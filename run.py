27623fb4a467679401317316f2fdb39a87150144
mccarvik <mccarviks@gmail.com>
2017-04-19 18:47:28 +0000

    added a path to find our libraries

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!flask/bin/python
 import os
 import sys
-from app import app
 # Need this to set up modules
 sys.path.append("/home/ubuntu/workspace/finance")
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+from app import app
+
 
 # Need this to reset DB
 os.system('sudo /etc/init.d/mysql restart')
