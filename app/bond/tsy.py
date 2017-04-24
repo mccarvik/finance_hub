569798aa7a898ee9ab384e203933d6f84a09bec4
mccarvik <mccarviks@gmail.com>
2017-04-24 01:19:44 +0000

    found the api, need to correct the query

--- app/bond/tsy.py
+++ app/bond/tsy.py
@@ -1,6 +1,10 @@
 # Need this to get the paths for the imports straight
 import sys
 sys.path.append("/home/ubuntu/workspace/finance")
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+import pdb, requests, datetime
+from app import app
+import pandas as pd
 
 def post(request):
     ''' Post method to receive all requests and send them to the appropriate method
@@ -21,6 +25,33 @@ def post(request):
         get_data()
 
 def get_data():
+    ''' retrieves raw data for treasuries through api 
+    
+    Parameters
+    ==========
+    NONE
+    
+    Return
+    ======
+    Pandas Array
+        raw data for the treasuries
+    '''
+    
+    # returns all securities with maturity date > today
+    url = 'https://www.treasurydirect.gov/TA_WS/securities/search?maturityDate >=today&format=json'
+    test_url = 'https://www.treasurydirect.gov/TA_WS/securities/TIPS?format=json'
+    try:
+        req = requests.get(url)
+    except Exception as e:
+        pdb.set_trace()
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        app.logger.info("API GRAB ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+    
+    tsy_df = pd.read_json(req.content.decode('utf-8'))
+    import pdb; pdb.set_trace()
     print("got here")
 
 
+# Used for testing
+if __name__ == '__main__':
+    get_data()
