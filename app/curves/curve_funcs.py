9dbbe7108a42dbd87e3448c337cac4f97489ff1f
mccarvik <mccarviks@gmail.com>
2017-04-28 14:50:31 +0000

    some notes

---
+++ app/curves/curve_funcs.py
@@ -0,0 +1,6 @@
+
+def loadTreasuryCurve():
+    ''' uses beautiful soup to scrape treasury xml for curve points'''
+    
+    url = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%204%20and%20year(NEW_DATE)%20eq%202017'
+    
