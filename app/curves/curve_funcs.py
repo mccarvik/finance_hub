adda0ed55f4099b5438b82b50b414d58cdf23fa0
mccarvik <mccarviks@gmail.com>
2017-04-30 00:38:01 +0000

    stubborn little load here, need to just get one load down so I can start codign from the general case

--- app/curves/curve_funcs.py
+++ app/curves/curve_funcs.py
@@ -1,22 +1,20 @@
 import sys
 sys.path.append("/usr/local/lib/python2.7/dist-packages")
 from bs4 import BeautifulSoup as bs
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+from lxml import etree
 import requests, re
 
 def loadTreasuryCurve():
     ''' uses beautiful soup to scrape treasury xml for curve points'''
     url = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%204%20and%20year(NEW_DATE)%20eq%202017'
     data = requests.get(url).text
-    root = ET.fromstring(data)
-    for child in root:
-        print(child.tag, child.attrib)
-    
-    for entry in root.findall('entry'):
-        print(entry.tag, entry.attrib)
-    
-    for entry in root.iter('entry'):
-        print(entry.tag, entry.attrib)
+    xml = bytes(bytearray(data, encoding='utf-8')) 
+    root = etree.XML(xml)
+    ns = "{" + root.xpath('namespace-uri(.)') + "}"
+    most_recent_curve = root.findall(ns + "entry")[-1].find(ns+'content')
+    # weird funky thing with the format, will try to solve later
+    # xml adds an xmlns namespace thing that latches on to each element
     
     import pdb; pdb.set_trace()
     print()
