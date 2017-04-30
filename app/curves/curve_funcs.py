88b49bd14bca59ca25aa0a8ff68b1df3efb07305
mccarvik <mccarviks@gmail.com>
2017-04-30 02:42:59 +0000

    finally got the load to kid of work

--- app/curves/curve_funcs.py
+++ app/curves/curve_funcs.py
@@ -9,12 +9,21 @@ def loadTreasuryCurve():
     ''' uses beautiful soup to scrape treasury xml for curve points'''
     url = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%204%20and%20year(NEW_DATE)%20eq%202017'
     data = requests.get(url).text
-    xml = bytes(bytearray(data, encoding='utf-8')) 
+    xml = bytes(bytearray(data, encoding='utf-8'))
     root = etree.XML(xml)
-    ns = "{" + root.xpath('namespace-uri(.)') + "}"
-    most_recent_curve = root.findall(ns + "entry")[-1].find(ns+'content')
     # weird funky thing with the format, will try to solve later
-    # xml adds an xmlns namespace thing that latches on to each element
+    # xml adds an xmlns namespace thing that latches on to each elements
+    ns = ["{"+n+"}" for n in list(root.nsmap.values())]
+    most_rec_curve = root.findall(ns[0] + "entry")[-1].find(ns[0]+'content')
+    most_rec_curve = most_rec_curve.find(ns[1]+'properties')
+    el_list = most_rec_curve.getchildren()
+    
+    id = el_list.pop(0).text
+    dt = el_list.pop(0).text
+    import pdb; pdb.set_trace()
+    rate_list = []
+    for el in el_list:
+        rate_list.append([el.tag.replace(ns[2],""), el.text])
     
     import pdb; pdb.set_trace()
     print()
