d4e62649cf3d8727665dd5cd6b1de81115cc3980
mccarvik <mccarviks@gmail.com>
2017-05-09 18:32:23 +0000

    equity started for reading 51

---
+++ app/equity/equity.py
@@ -0,0 +1,10 @@
+
+
+class Equity():
+    '''This class will represent an individual stock'''
+    
+    def __init__(self, div_yld=0.0):
+        '''Constructor'''
+        
+        self._div_yld = div_yld
+
