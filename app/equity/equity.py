0cb8632b5bbf765dc500df73125574805dda4eba
mccarvik <mccarviks@gmail.com>
2017-05-10 01:54:52 +0000

    gonna make some additions to the equity calcs and keep working on html side as well

--- app/equity/equity.py
+++ app/equity/equity.py
@@ -3,11 +3,12 @@
 class Equity():
     '''This class will represent an individual stock'''
     
-    def __init__(self, div_yld=0.0):
+    def __init__(self, div_yld=0.0, div_freq=0.25):
         '''Constructor'''
         
         self._div_yld = div_yld
+        self._div_freq = div_freq
 
     
-    def calcDividendDiscountModel(self, r_req):
+    def calcDividendDiscountModel(self, r_req, hold_per=1.0):
         return 0
