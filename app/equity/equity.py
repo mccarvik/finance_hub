945605ddf1e93df1434049696baeb781ba699b33
mccarvik <mccarviks@gmail.com>
2017-05-10 17:57:45 +0000

    DDM done

--- app/equity/equity.py
+++ app/equity/equity.py
@@ -10,19 +10,35 @@ class Equity():
     '''This class will represent an individual stock'''
     
     def __init__(self, div_yld=0.0, div_freq=0.25, cur_px=100,
-                trade_dt=datetime.date.today(), divs=None):
+                trade_dt=datetime.date.today()):
         '''Constructor'''
         
         self._div_yld = div_yld
         self._div_freq = div_freq
         self._cur_px = cur_px
         self._trade_dt = trade_dt
-        self._divs = np.array(divs) if divs else np.array([])
         
 
-    
     def calcDividendDiscountModel(self, r_req, hold_per, sale_px, divs=False):
-        pdb.set_trace()
+        ''' Calculates the value of the stock based on the Dividend
+        Discount Model
+        
+        Parameters
+        ==========
+        r_req : float
+            The required rate of return
+        hold_per : float
+            The holding period for the investment, expressed in years
+        sale_px : float
+            assumed price at sale of investment
+        divs : list of floats
+            the dividends if explicitly given
+            
+        Return
+        ======
+        pv : float
+            The assumed present value based on DDM
+        '''
         if not divs and not self._divs:
             div_flows = np.array(createCashFlows(self._trade_dt, self._div_freq,
                         self._trade_dt + datetime.timedelta(365*hold_per),
@@ -38,8 +54,6 @@ class Equity():
         pv += calcPV(sale_px, r_req*self._div_freq, hold_per/self._div_freq)
         return pv        
         
-        return 0
-
 if __name__ == "__main__":
     e = Equity(div_yld=0.02, div_freq=1, trade_dt=datetime.date(2017,1,1))
     divs = [2, 2.1, 2.2]
