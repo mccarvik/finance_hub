2c8d7435bba4534e4d7d3bd4e3cc8be58bc777ae
mccarvik <mccarviks@gmail.com>
2017-05-16 15:16:43 +0000

    getting going on morningstar

--- app/equity/data_grab/morningstar_dg.py
+++ app/equity/data_grab/morningstar_dg.py
@@ -7,27 +7,40 @@ from threading import Thread
 from app import app
 
 def getData():
-    
-    # Getting all the tickers
+    # Getting all the tickers from text file
     tasks = []
     with open("/home/ubuntu/workspace/finance/app/equity/screener_eqs/memb_list.txt", "r") as f:
-        ct = 0
-        tickers = ""
         for line in f:
-            tickers += line.strip() + "+"
-            ct += 1
-            if ct == 20:
-                tickers = tickers[:-1]
-                tasks.append(tickers)
-                tickers = ""
-                ct = 0
-        else:
-            tickers = tickers[:-1]
-            tasks.append(tickers)
+            tasks.append(line.strip())
+    
+    threads = []
+    try:
+        # for running multithreaded: starts the thread and 'joins it' so we will wait for all to finish
+        # for t in tasks:
+        #     threads.append(Thread(target=makeAPICall, args=(t,)))
+        # [t.start() for t in threads]
+        # [t.join() for t in threads]
+        
+        # for running single threaded
+        for t in tasks:
+            makeAPICall(t)
+            
+    except Exception as e:
+        import pdb; pdb.set_trace()
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        app.logger.info("Error in async loop: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+    t1 = time.time()
+    app.logger.info("Done Retrieving data, took {0} seconds".format(t1-t0))
 
-
-
-
+def makeAPICall(tick):
+    url = 'http://financials.morningstar.com/ajax/exportKR2CSV.html?t=' + tick
+    req = requests.get(url)
+    # df = pd.read_csv(req.content.decode('utf-8'))
+    content = req.content.decode('utf-8')
+    import pdb; pdb.set_trace()
+    cr = csv.reader(content.splitlines(), delimiter=',')
+    print("")
+    
 
 
 
