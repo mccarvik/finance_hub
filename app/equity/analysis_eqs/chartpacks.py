ce1a39c6495fcb167535786bc71b90d0733857f0
mccarvik <mccarviks@gmail.com>
2017-05-23 17:07:39 +0000

    small update

--- app/equity/analysis_eqs/chartpacks.py
+++ app/equity/analysis_eqs/chartpacks.py
@@ -3,11 +3,12 @@ sys.path.append("/home/ubuntu/workspace/finance")
 sys.path.append("/usr/local/lib/python2.7/dist-packages")
 import matplotlib as mpl
 mpl.use('Agg')
+import datetime
 from pandas_datareader.data import DataReader
 from app.equity.analysis_eqs.utils_analysis import getKeyStatsDataFrame
 
 
-def mvg_avgs(tickers, duration):
+def mvg_avgs(tickers, duration, date=datetime.date.today()):
     tickers.append('^GSPC')
     market = DataReader(tickers, 'yahoo', sdate, edate,pause=1)['Close']
     df['50DayMvgAvg'] = df.apply(lambda x: mv_avg_50[mv_avg_50['Date'] >= datetime.date(int(x.name[1]), \
@@ -20,7 +21,7 @@ def run(tickers, date, cp):
     ms_df = getKeyStatsDataFrame(tickers=tickers, table="morningstar")
     pngs = []
     if cp == "CP1":
-        png.append(mvg_avgs(ticker, 5))
+        png.append(mvg_avgs(ticker, 5, date))
 
 
 
